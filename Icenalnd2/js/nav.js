fb8b7431da58ac85622991d1c84ae4495c4a6b85
vargaalex1 <105971545+vargaalex1@users.noreply.github.com>
2022-05-21 13:05:15 +0200

    Delete Icenalnd2 directory

--- Icenalnd2/js/nav.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body'),
-      sidebar = body.querySelector('.sidebar'),
-      toggle = body.querySelector('.toggle'),
-      rightSide = body.querySelector('.right-side');
-
-toggle.addEventListener('click', () => {
-    sidebar.classList.toggle('close')
-});
